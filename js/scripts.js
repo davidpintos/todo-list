7ea42d3310f566b70aae2c3a6ffe867f93ccaa14
David Pintos <d.pintos@zakeke.com>
2020-10-20 21:06:50 -0300

    Adding bootstrap and improving the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
+const checkEnterKey = (ev) => {
+    if (ev.keyCode === 13) {
+        createTask();
+    }
+}
+
 const createTask = () => {
     // obtenemos el campo que tiene el nombre de la tarea
     const taskField = document.getElementById('task-name');
     const tasksList = document.getElementById('tasks-list');
     
-    tasksList.innerHTML = tasksList.innerHTML + '<li>' + taskField.value + '</li>';
+    if (taskField.value.trim() !== "") {
+        tasksList.innerHTML = tasksList.innerHTML + '<li>' + taskField.value + '</li>';
+    }
 }
