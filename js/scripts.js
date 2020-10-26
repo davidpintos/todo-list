a6607e695702af79281a020c14c39b7bb1d9c2a8
David Pintos <d.pintos@zakeke.com>
2020-10-26 20:51:05 -0300

    Marking tasks as Done and Removed

--- js/scripts.js
+++ js/scripts.js
@@ -23,19 +23,14 @@ const createTask = () => {
         const task = {
             id: ids,
             description: taskField.value,
-            status: 'in-progress', // 'done'
+            status: 'in-progress',
         };
 
         // Agregamos la tarea al array
         tasks.push(task);
+        
+        showTasksList();
 
-        tasksList.innerHTML = `${tasksList.innerHTML}
-            <li class="pt-3">
-                ${task.description}
-                <button class="btn btn-success" onclick="markAsDone(${task.id})">Marcar como Terminada</button>
-                <button class="btn btn-danger" onclick="remove(${task.id})">Eliminar</button>
-            </li>
-        `;
         total.innerHTML = tasks.length;
         taskField.value = '';
     } else {
@@ -43,14 +38,66 @@ const createTask = () => {
     }
 }
 
+/**
+ * Muestra la lista de tareas: 
+ * A - Si una tarea está en estado "removed"
+ * no la agrega a la lista.
+ * B - Si una tarea está en estado "done", 
+ * le pone un estilo diferente para mostrar que está "lista".
+ */
 const showTasksList = () => {
+    const tasksList = document.getElementById('tasks-list');
+    let content = '';
 
+    tasks.forEach((task) => {
+        let statusText = '';
+        let statusClass = '';
+        let buttons = '';
+
+        if (task.status === 'done') {
+            statusText = '(terminada!)';
+            statusClass = 'text-success';
+        }
+
+        if (task.status === 'removed') {
+            statusText = '(eliminada!)';
+            statusClass = 'text-danger';
+        } 
+        
+        if (task.status === 'in-progress') 
+        {
+            buttons = `<button class="btn btn-success" onclick="markAsDone(${task.id})">Marcar como Terminada</button>
+            <button class="btn btn-danger" onclick="remove(${task.id})">Eliminar</button>`;
+        }
+
+        content = `${content}
+            <li class="pt-3 ${statusClass}">
+                ${task.description} ${statusText}
+                ${buttons}
+            </li>`;
+    });
+
+    tasksList.innerHTML = content;
+   
 }
 
 const markAsDone = (taskId) => {
+    tasks.forEach((task) => {
+        if (taskId === task.id) {
+            task.status = 'done';
+        }
+    });
 
+    showTasksList();
 }
 
 const remove = (taskId) => {
-    //console.log(taskId);
+
+    tasks.forEach((task) => {
+        if (taskId === task.id) {
+            task.status = 'removed';
+        }
+    });
+
+    showTasksList();
 }
